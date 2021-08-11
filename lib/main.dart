1e54d9b212aba7447db2a59c54f09e8283755d66
hrishik18 <hrishik.s@somaiya.edu>
2021-08-11 16:22:02 +0530

    Title changed

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Building Messenger App'),
+      home: MyHomePage(title: 'Building Messenger App!'),
     );
   }
 }
