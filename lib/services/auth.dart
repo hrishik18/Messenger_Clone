90160b665a958e1836b777c5b5991adbcbc617fd
hrishik18 <hrishik.s@somaiya.edu>
2021-08-11 21:19:11 +0530

    Added views and servies

---
+++ lib/services/auth.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthMethods {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  getCurrentUser() {
+    return auth.currentUser;
+  }
+
+  signInWithGoogle(BuildContext context) async{
+    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount googleSignInAccount = await  _googleSignIn.signIn();
+  }
