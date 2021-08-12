0bba28e45a6cbe933ea0495b0d47e6f314bfecdf
hrishik18 <hrishik.s@somaiya.edu>
2021-08-12 16:28:55 +0530

    Auth error changd

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -12,6 +12,19 @@ class AuthMethods {
   signInWithGoogle(BuildContext context) async{
     final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
     final GoogleSignIn _googleSignIn = GoogleSignIn();
+     
+     
+    final  GoogleSignInAccount? googleSignInAccount =///////////////////yaha par bhi 
+        await _googleSignIn.signIn();
 
-    final GoogleSignInAccount googleSignInAccount = await  _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount!.authentication;///////////////////////////////////////////yaha par . ke phele
+    final AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    UserCredential result =
+        await _firebaseAuth.signInWithCredential(credential);
+    User userDetails = result.user!;   ///////////user ke baadh 
   }
+}
