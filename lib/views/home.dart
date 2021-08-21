141e958156d8ea8e8dd70350f7fda801c6980e18
hrishik18 <hrishik.s@somaiya.edu>
2021-08-21 22:31:27 +0530

    Search implemented

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -11,6 +11,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isSearching = false;
+  TextEditingController searchUsernameEditingController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,41 +24,61 @@ class _HomeState extends State<Home> {
           InkWell(
             onTap: () {
               AuthMethods().signOut().then((s) {
-               // Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => SignIn() ));
-               Get.to(SignIn());   
-            });
+                // Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => SignIn() ));
+                Get.to(SignIn());
+              });
             },
             child: Container(
                 padding: EdgeInsets.symmetric(horizontal: 16),
                 child: Icon(Icons.exit_to_app)),
-                )      
+          )
         ],
       ),
       body: Container(
-        margin: EdgeInsets.symmetric(horizontal:20),
+        margin: EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16),
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              decoration:BoxDecoration(
-                border:   Border.all(
-                  color: Colors.black87,
-                  width: 1.0,
-                  style:BorderStyle.solid),
-                  borderRadius:BorderRadius.circular(24),
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    child: Expanded(
-                    child: TextField(
-                      decoration:InputDecoration(
-                        border:InputBorder.none,hintText:"username"),
-                    )),
-                  ),          
-                Icon(Icons.search),
-                ],
+            isSearching
+                ? GestureDetector(
+                    onTap: () {
+                      isSearching = false;
+                      searchUsernameEditingController.text = "";
+                      setState(() {});
+                    },
+                    child: Padding(
+                        padding: EdgeInsets.only(right: 12),
+                        child: Icon(Icons.arrow_back)),
+                  )
+                : Container(),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 16),
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.black87,
+                      width: 1.0,
+                      style: BorderStyle.solid),
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Expanded(
+                          child: TextField(
+                        controller: searchUsernameEditingController,
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: "username"),
+                      )),
+                    ),
+                    GestureDetector(
+                        onTap: () {
+                          isSearching = !isSearching;
+                          setState(() {});
+                        },
+                        child: Icon(Icons.search)),
+                  ],
+                ),
               ),
             )
           ],
