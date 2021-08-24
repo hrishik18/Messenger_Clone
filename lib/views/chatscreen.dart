216a4d3a3768d41cbdddd70bc4b06ce72aee5ddf
hrishik18 <hrishik.s@somaiya.edu>
2021-08-24 22:27:38 +0530

    Starting chat screen

--- lib/views/chatscreen.dart
+++ lib/views/chatscreen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
-class ChatScreen extends StatefulWidget {
-  const ChatScreen({ Key? key }) : super(key: key);
 
+class ChatScreen extends StatefulWidget {
+  final String chatWithUser,name ;
+  ChatScreen(this.chatWithUser,this.name)
+  //const ChatScreen({Key? key}) : super(key: key);
+  
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
+
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
+    return Scaffold();
   }
 }
