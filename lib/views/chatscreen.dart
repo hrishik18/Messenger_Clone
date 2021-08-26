b6759b16541368df0a7804cde060a9670a4b35d1
hrishik18 <hrishik.s@somaiya.edu>
2021-08-26 20:10:28 +0530

    chat screen made

--- lib/views/chatscreen.dart
+++ lib/views/chatscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ChatScreen extends StatefulWidget {
   final String chatWithUser,name ;
@@ -11,8 +12,52 @@ class ChatScreen extends StatefulWidget {
 
 
 class _ChatScreenState extends State<ChatScreen> {
+  late String chatRoomId,messageId="";
+  late String myName,myProfilePic,myUserName,myEmail;
+
+  grtMyInfoFromSP() async{
+    myName= await SharedPreferencesHelper().getDisplayName();
+    myProfilePic = await SharedPreferencesHelper().getUserProfileUrl();
+    myUserName=await SharedPreferencesHelper().getUserName();
+    myEmail=await SharedPreferencesHelper().getUserEmail();
+
+    chatRoomId=getchatid(widget.chatWithUser,myUserName);
+    }
+    getchatid(String a,String b){
+      if(a.substring(0,1).codeUnitAt(0)>b.substring(0,1).codeUnitAt(0))
+      return "$b\_$a";
+      else
+      return "$a/$b";
+    }
+    getandsetMessage()async{
+
+
+    }
+    dothisonlaunch() async{
+       await  getInfoFromSharedPrefrences();
+       getandsetMessage();
+    }
   @override
+  void initState(){
+    dothisonlaunch();
+    super.initState();
+  }
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.name),
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              child: Row(
+                children: [Expanded(child:TextField()),Icon(Icons.send)],
+                ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
